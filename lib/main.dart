7e7ac1feb4f4ff27a2db01514b7073c923ade293
COLLINS <ccollins2030cc@gmail.com>
2021-11-16 16:50:49 +0300

    Firestore

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:firebaseauth/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Firebase Auth'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ChatScreen(),
+
+    );
+  }
+}
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (ctx, index) => Container(
+                  padding: EdgeInsets.all(8),
+                  child: Text("This works"),
+                )),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              Firebase.initializeApp();
+              FirebaseFirestore.instance
+                  .collection('chats/E0qkSvEHkrDL11BfgKuQ')
+                  .snapshots()
+                  .listen((event) {
+                print(event);
+              });
+            }));
+  }
+}
